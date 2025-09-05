1155979fb55cb364c120f6293f9e32bbe756794c
Dan Nelson <dan.nelson1@gmail.com>
2025-09-05 13:57:46 -0700

    initial  commit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # empty/main.py
 
+# import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+from fake_useragent import UserAgent
+
+# import duckdb
+import requests
+
+url = "https://en.wikipedia.org/wiki/U.S._state"
+ua = UserAgent()
+headers = {"user-agent": ua.random}
+
+page = requests.get(url, headers=headers)
+soup = bs(page.content, "html.parser")
+title = soup.find("title")
+print(title)
